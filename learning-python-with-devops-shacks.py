a1c08f24b51b0ac746c4632ac9ac1a4152c9ef11
Cikaylanre <ray@Cikay.local>
2024-02-06 22:40:05 +0100

    I'm trying to learn how to commit into the github

---
+++ learning-python-with-devops-shacks.py
@@ -0,0 +1,7 @@
+names = ["Deola", "Bash", "Nola", 76, 67.4, "Bash", 5657j]
+ded = names[0:4]
+print(ded)
+
+student_scores = {"Deola": {"Maths": 67, "Eng": 89, "Soc": 90},
+                  "Temmy": {"Maths": 80, "Eng": 90, "Soc": 95}}
+print(student_scores)
